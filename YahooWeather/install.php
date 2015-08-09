04041cc58c1f856b2e9e7f220490333a9ddc95f8
edmonsur <edmonsur@users.noreply.github.com>
2015-08-09 22:27:42 +0200

    Update install.php

--- YahooWeather/install.php
+++ YahooWeather/install.php
@@ -13,6 +13,18 @@
 
 function YahooWeather_install()
 {
+	/** Create yahooweather tabelle  */
+			$query = file_get_contents('modules/YahooWeather/database/mysql/create/create_YahooWeather.mysql');
+			if ($query)
+			{
+				if ($this->_config->get('dbPrefix'))
+				{
+					$query = str_replace($this->_prefixPlaceholder, $this->_config->get('dbPrefix'), $query);
+				}
+	Redaxscript\Db::rawExecute($query);
+			}
+	/** Create yahooweather tabelle  */
+	
 	Redaxscript\Db::forTablePrefix('modules')
 		->create()
 		->set(array(
@@ -23,7 +35,11 @@ function YahooWeather_install()
 			'version' => '2.4.0'
 		))
 		->save();
-}
+
+		
+		
+		
+		}
 
 /**
  * contact uninstall
@@ -31,10 +47,25 @@ function YahooWeather_install()
  * @since 1.2.1
  * @deprecated 2.0.0
  *
+ * @package Redaxscript
+ * @category Modules
+ * @author  edmonsur
  */
 
 function YahooWeather_uninstall()
 {
 	Redaxscript\Db::forTablePrefix('modules')->where('alias', 'YahooWeather')->findMany()->delete();
+
+		/** Drop yahooweather tabelle  */
+			$query = file_get_contents('modules/YahooWeather/database/mysql/drop/drop_YahooWeather.mysql');
+			if ($query)
+			{
+				if ($this->_config->get('dbPrefix'))
+				{
+					$query = str_replace($this->_prefixPlaceholder, $this->_config->get('dbPrefix'), $query);
+				}
+	Redaxscript\Db::rawExecute($query);
+			}
+	/** Drop yahooweather tabelle  */
 }
 
